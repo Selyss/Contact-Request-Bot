9dec086f498cfabea74adf981d37ad67072312dc
Selyss <99344963+Selyss@users.noreply.github.com>
2023-11-26 21:37:40 -0500

    channel maker

--- cogs/ticket.py
+++ cogs/ticket.py
@@ -3,6 +3,9 @@ from nextcord.ext.commands import Bot, Cog
 import nextcord
 import json
 
+# TODO: add to config
+TICKET_CATEGORY: int = 1178415757378461727
+
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
 
@@ -33,6 +36,16 @@ class Ticket(Cog):
             f"Request channel set to <#{request_channel.id}>. Advertisement channel set to <#{ad_channel.id}>.",
             ephemeral=True,
         )
+
+    async def create_ticket(self, inter: nextcord.Interaction) -> None:
+        category = nextcord.utils.get(inter.guild.categories, id=TICKET_CATEGORY)
+        new_channel = await category.create_text_channel(
+            inter.user.name,
+            overwrites=None,
+            reason=f"Made ticket for {inter.user.name}",
+        )
+        await new_channel.send("This is your new channel!")
+        await interaction.response.send_message(f"Channel created: <#{new_channel.id}>")
 
 
 def setup(bot: Bot) -> None:
